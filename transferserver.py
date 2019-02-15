e7033c352d9c698d2b727a30a696160fcfc7ba30
Heju <heju@Hejus-MBP.lan>
2019-02-15 23:41:53 +0100

    TransferServer code cleaned

--- transferserver.py
+++ transferserver.py
@@ -6,15 +6,13 @@ host = "192.168.1.123"
 print("Host: ", host)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
-s.listen(1)
+s.listen(2)
 
 print("Server Listening...")
 
 while True:
     conn, addr = s.accept()
     print("Got Connection from ", addr)
-#    data = conn.recv(1024)
-#   print("Server Received ", repr(data))
     with open("Moneyball.mp4", 'wb') as f:
         while True:
             data = conn.recv(1024)
@@ -23,16 +21,6 @@ while True:
             f.write(data)
 
     f.close()
-
-    #vvv file sending code
-    #filename = "mytxt.txt"
-    #f = open(filename, 'rb')
-    #l = f.read(1024)
-    #while (l):
-     #   conn.send(l)
-     #   print("Sent ", repr(l))
-     #   l = f.read(1024)
-    #f.close()
 
     print("Done Sending!")
     conn.send("File Received")
