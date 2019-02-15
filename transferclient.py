d7dcbae0dcd17c1b20965ee77e3fd2fff1eb1890
Elsie Kim <lizzy@Elsies-MacBook.lan>
2019-02-15 22:33:37 +0100

    Transfer Client for Testing

---
+++ transferclient.py
@@ -0,0 +1,23 @@
+import socket
+
+s = socket.socket()
+host = "192.168.1.123"
+port = 60000
+
+s.connect((host, port))
+s.send("Hello Server!")
+
+with open('received_file', 'wb') as f:
+    print("File Opened")
+    while True:
+        print("Receiving data...")
+        data = s.recv(1024)
+        print ("data=%s", (data))
+        if not data:
+            break
+        f.write(data)
+
+f.close()
+print("Successfully got the file")
+s.close()
+print("Connection Closed")
